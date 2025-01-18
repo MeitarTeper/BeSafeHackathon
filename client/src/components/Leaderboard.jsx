51e0a9e742cac2638b6d847a10a2ccee0cd4f3ce
Noamos321 <noamos321@gmail.com>
2025-01-18 18:54:13 +0200

    Update Leaderboard.jsx

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types"; // ייבוא PropTypes לצורך ולידציה
 import { getScores } from "../services/score_api";
 
 const Leaderboard = (props) => {
@@ -15,21 +16,26 @@ const Leaderboard = (props) => {
     return (
         <table className="LeaderboardTable">
             <tbody>
-            <tr>
-                <th>No.</th>
-                <th>Name</th>
-                <th>Score</th>
-            </tr>
-            {scores.map((score, index) => (
-                <tr key={score.id}>
-                    <td>{index + 1}</td>
-                    <td>{score.playerName}</td>
-                    <td>{score.score}</td>
+                <tr>
+                    <th>No.</th>
+                    <th>Name</th>
+                    <th>Score</th>
                 </tr>
-            ))}
+                {scores.map((score, index) => (
+                    <tr key={score.id}>
+                        <td>{index + 1}</td>
+                        <td>{score.playerName}</td>
+                        <td>{score.score}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
+};
+
+// הגדרת PropTypes לוודא שהפרופ gameName תקין
+Leaderboard.propTypes = {
+    gameName: PropTypes.string.isRequired, // הגדרה ש- gameName חייב להיות מחרוזת חובה
 };
 
 export default Leaderboard;
